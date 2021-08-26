2518452ac1d8131227a8fa3fd20d7515b717e7ff
Wagner Xu <wagnerhsu@hotmail.com>
2021-08-26 14:12:41 +0800

    Update project files

--- Example/Text/ClassLibraryDevelopers/LanguageProvider.cs
+++ Example/Text/ClassLibraryDevelopers/LanguageProvider.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace ClassLibraryDevelopers
 {
@@ -9,38 +6,38 @@ namespace ClassLibraryDevelopers
     /// A simplified provider for translated strings.
     /// </summary>
     /// <remarks>A real implementation could for instance provide caching.</remarks>
-public class LanguageProvider
-{
-    private static List<ILanguageProvider> _providers = new List<ILanguageProvider>();
-    private static ILanguageProvider _default = new StringTableProvider(InternalResources.ResourceManager);
-
-    /// <summary>
-    /// Gets the string.
-    /// </summary>
-    /// <param name="name">The name.</param>
-    /// <returns>Localized string if found; otherwise the requested name.</returns>
-    public static string GetString(string name)
+    public class LanguageProvider
     {
-        var providers = _providers;
-        foreach (var provider in providers)
-        {
-            var str = provider.GetString(name);
-            if (str != null)
-                return str;
+        private static List<ILanguageProvider> _providers = new List<ILanguageProvider>();
+        private static ILanguageProvider _default = new StringTableProvider(InternalResources.ResourceManager);
 
+        /// <summary>
+        /// Gets the string.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>Localized string if found; otherwise the requested name.</returns>
+        public static string GetString(string name)
+        {
+            var providers = _providers;
+            foreach (var provider in providers)
+            {
+                var str = provider.GetString(name);
+                if (str != null)
+                    return str;
+
+            }
+
+            return _default.GetString(name) ?? name;
         }
 
-        return _default.GetString(name) ?? name;
+        /// <summary>
+        /// Register a new provider.
+        /// </summary>
+        /// <param name="provider"></param>
+        public static void Register(ILanguageProvider provider)
+        {
+            var providers = new List<ILanguageProvider>(_providers) { provider };
+            _providers = providers;
+        }
     }
-
-    /// <summary>
-    /// Register a new provider.
-    /// </summary>
-    /// <param name="provider"></param>
-    public static void Register(ILanguageProvider provider)
-    {
-        var providers = new List<ILanguageProvider>(_providers) {provider};
-        _providers = providers;
-    }
-}
 }
--- Example/Text/ClassLibraryDevelopers/Program.cs
+++ Example/Text/ClassLibraryDevelopers/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ClassLibraryDevelopers
 {
     class Program
     {
-static void Main(string[] args)
-{
-    Console.WriteLine(LanguageProvider.GetString("SomeName"));
-}
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LanguageProvider.GetString("SomeName"));
+        }
     }
 }
